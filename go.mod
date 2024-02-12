d0e1ddd1b57dfc3f618c17d2dd3983a951356a76
inuoshios <inuoshios@gmail.com>
2024-02-13 00:49:43 +0100

    feat: upgrade to go1.22.0
    remove all external packages and moved all files to main.go

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/inuoshios/nigeria-uni
 
-go 1.19
-
-require github.com/gorilla/mux v1.8.0
+go 1.22
--- main.go
+++ main.go
@@ -1,20 +1,122 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
-
-	"github.com/inuoshios/nigeria-uni/router"
+	"os"
+	"strings"
 )
+
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+func (h HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h(w, r)
+}
+
+type Universities struct {
+	ID           uint   `json:"id"`
+	Name         string `json:"name"`
+	Abbreviation string `json:"abbreviation"`
+	WebsiteLink  string `json:"website_link"`
+}
+
+// CovertJSONToStruct reads the JSON file and converts it to a struct...
+func CovertJSONToStruct() []Universities {
+	uni, err := os.ReadFile("json/uni.json")
+	if err != nil {
+		log.Fatalf("an error occured: %v", err)
+	}
+
+	var data []Universities
+
+	if err = json.Unmarshal(uni, &data); err != nil {
+		log.Fatalf("an error occured: %v", err)
+	}
+
+	return data
+}
+
+func AddContentType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	welcome := map[string]any{
+		"status": 200,
+		"title":  "List of all univeristies in Nigeria—still in progress.",
+		"msg":    "PRs are allowed. 🪢",
+	}
+	json.NewEncoder(w).Encode(welcome)
+}
+
+func GetAllUniveristy(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	var universities []Universities
+	for index, data := range CovertJSONToStruct() {
+		universities = append(universities, Universities{
+			ID:           uint(index + 1),
+			Name:         data.Name,
+			Abbreviation: data.Abbreviation,
+			WebsiteLink:  data.WebsiteLink,
+		})
+	}
+
+	if err := json.NewEncoder(w).Encode(universities); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "An error occurred",
+		})
+		return
+	}
+}
+
+func GetSpecificUniversity(w http.ResponseWriter, r *http.Request) {
+	param := r.PathValue("abbreviation")
+
+	for index, data := range CovertJSONToStruct() {
+		data.ID = uint(index + 1)
+		if strings.EqualFold(data.Abbreviation, param) {
+			if err := json.NewEncoder(w).Encode(data); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusBadRequest)
+				json.NewEncoder(w).Encode(map[string]string{
+					"message": "An error occurred",
+				})
+				return
+			}
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Error finding a university with that abbreviation",
+	})
+}
+
+func routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /api", AddContentType(HandlerFunc(Welcome)))
+	mux.Handle("GET /api/v1", AddContentType(HandlerFunc(GetAllUniveristy)))
+	mux.Handle("GET /api/v1/{abbreviation}", AddContentType(HandlerFunc(GetSpecificUniversity)))
+
+	return mux
+}
 
 func main() {
 	port := "8000"
 
 	log.Println("server running at port", port)
 
-	route := router.NEW()
-
-	if err := http.ListenAndServe(":"+port, route); err != nil {
+	if err := http.ListenAndServe(":"+port, routes()); err != nil {
 		log.Fatal(err)
 	}
 }
